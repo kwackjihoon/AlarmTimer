166815682c9273d6c71e77e52745d78cc1a1b586
L703 <l703@L703ui-iMac-45.local>
2018-04-30 16:16:33 +0900

    AlarmTimer

--- AlarmTimer/ViewController.swift
+++ AlarmTimer/ViewController.swift
@@ -9,17 +9,53 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var lblCurrent: UILabel!
+    @IBOutlet weak var lblAlarmTime: UILabel!
+    @IBOutlet weak var myDataPicker: UIDatePicker!
+    
+    var myTimer = Timer()
+    
+    func updateTime() {
+        let date = Date()
+        let formatter = DateFormatter()
+        formatter.dateFormat = "hh:mm:ss"
+        
+        lblCurrent.text = formatter.string(from: date)
+        
+        if lblCurrent.text == lblAlarmTime.text{
+            view.backgroundColor = UIColor.red
+        }
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        let formatter = DateFormatter()
+        formatter.dateFormat = "hh:mm:ss"
+        
+        lblAlarmTime.text = formatter.string(from: myDataPicker.date)
+        
+        myTimer = Timer.scheduledTimer(withTimeInterval: 1, repeats: true, block: { (myTimer) in
+            self.updateTime()
+        })
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+        
+    @IBAction func DataPicker(_ sender: Any) {
+        
+        print("change Date Picker")
+        print(myDataPicker.date)
+        
+        let formatter = DateFormatter()
+        formatter.dateFormat = "hh:mm:ss"
+        lblAlarmTime.text = formatter.string(from: myDataPicker.date)
+        
     }
-
+    
+    @IBAction func btReset(_ sender: Any) {
+        view.backgroundColor = UIColor.white
+    }
+    
 
 }
 
